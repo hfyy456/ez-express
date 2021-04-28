a5d57ed22c0569d2e914b2976cd9f0670452ad4c
皇甫渊源 <540363975@qq.com>
2021-04-28 20:00:43 +0800

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -79,6 +79,26 @@ router.post('/info', jsonParser, (req, res) => {
         }
     });
 })
+router.post('/list', jsonParser, (req, res) => {
+    console.log(req.user.username)
+    orderdao.findAll({
+        owner: req.user.username
+    }).then((result) => {
+        if (result) {
+            res.json({
+                code: 20000,
+                data: result,
+                message: "查询成功"
+            })
+        } else {
+            res.json({
+                code: 50000,
+                data: result,
+                message: "查询失败"
+            })
+        }
+    })
+})
 router.post("/pay", jsonParser, (req, res) => {
     console.log(req.user)
     console.log(req.user.username)
